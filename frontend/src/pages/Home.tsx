e353255cbdbacc8479827fbf0df7603408557934
Alaeddinesoufiane12 <alaeddinesoufiane5@gmail.com>
2025-09-05 09:02:49 +0200

    front

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,12 +3,12 @@ import UploadCard from '../components/UploadCard';
 
 const Home: React.FC = () => {
   return (
-    <div className="container mt-4">
+  <div className="container mt-4 text-read">
       <div className="row">
         <div className="col-md-8 mx-auto">
-          <div className="text-center mb-4">
-            <h1>ASL Hand Sign Classifier</h1>
-            <p className="lead text-muted">
+          <div className="text-center mb-4 text-read">
+            <h1 className="text-read">ASL Hand Sign Classifier</h1>
+            <p className="lead text-read">
               Upload an image of an ASL hand sign to get instant predictions using our VGG16 neural network.
             </p>
           </div>
@@ -18,7 +18,7 @@ const Home: React.FC = () => {
           <div className="mt-5">
             <div className="card">
               <div className="card-body">
-                <h5 className="card-title">How it works</h5>
+                <h5 className="card-title text-read" style={{ color: '#fff' }}>How it works</h5>
                 <div className="row">
                   <div className="col-md-4 text-center">
                     <div className="mb-3">
@@ -27,8 +27,8 @@ const Home: React.FC = () => {
                         <path fillRule="evenodd" d="M7.646 4.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V14.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3z"/>
                       </svg>
                     </div>
-                    <h6>1. Upload</h6>
-                    <p className="text-muted small">
+                    <h6 className="text-read" style={{ color: '#fff' }}>1. Upload</h6>
+                    <p className="text-read" style={{ color: '#fff' }}>
                       Upload or drag & drop an image of an ASL hand sign
                     </p>
                   </div>
@@ -38,8 +38,8 @@ const Home: React.FC = () => {
                         <path d="M5 0a.5.5 0 0 1 .5.5V2h1V.5a.5.5 0 0 1 1 0V2h1V.5a.5.5 0 0 1 1 0V2h1V.5a.5.5 0 0 1 1 0V2A2.5 2.5 0 0 1 14 4.5h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14v1h1.5a.5.5 0 0 1 0 1H14a2.5 2.5 0 0 1-2.5 2.5v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14h-1v1.5a.5.5 0 0 1-1 0V14A2.5 2.5 0 0 1 2 11.5H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2v-1H.5a.5.5 0 0 1 0-1H2A2.5 2.5 0 0 1 4.5 2V.5A.5.5 0 0 1 5 0zm-.5 3A1.5 1.5 0 0 0 3 4.5v7A1.5 1.5 0 0 0 4.5 13h7a1.5 1.5 0 0 0 1.5-1.5v-7A1.5 1.5 0 0 0 11.5 3h-7zM5 6.5A1.5 1.5 0 0 1 6.5 5h3A1.5 1.5 0 0 1 11 6.5v3A1.5 1.5 0 0 1 9.5 11h-3A1.5 1.5 0 0 1 5 9.5v-3zM6.5 6a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z"/>
                       </svg>
                     </div>
-                    <h6>2. Analyze</h6>
-                    <p className="text-muted small">
+                    <h6 className="text-read" style={{ color: '#fff' }}>2. Analyze</h6>
+                    <p className="text-read" style={{ color: '#fff' }}>
                       Our VGG16 model processes the image and identifies the sign
                     </p>
                   </div>
@@ -49,8 +49,8 @@ const Home: React.FC = () => {
                         <path fillRule="evenodd" d="M0 0h1v15h15v1H0V0Zm14.817 3.113a.5.5 0 0 1 .07.704l-4.5 5.5a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61 4.15-5.073a.5.5 0 0 1 .704-.07Z"/>
                       </svg>
                     </div>
-                    <h6>3. Results</h6>
-                    <p className="text-muted small">
+                    <h6 className="text-read" style={{ color: '#fff' }}>3. Results</h6>
+                    <p className="text-read" style={{ color: '#fff' }}>
                       Get top 3 predictions with confidence scores
                     </p>
                   </div>
